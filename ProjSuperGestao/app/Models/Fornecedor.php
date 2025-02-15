2df159d67943af8fbac2587609639acfb8f8072c
André Torres <150715327+andtors@users.noreply.github.com>
2025-02-15 18:30:33 -0300

    SC-8-101

--- ProjSuperGestao/app/Models/Fornecedor.php
+++ ProjSuperGestao/app/Models/Fornecedor.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// fornecedores
+
 class Fornecedor extends Model
 {
     use HasFactory;
+    protected $table = 'fornecedores';
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
+
+$contato4 = SiteContato::where('nome', '!=', 'Fernando')->whereIn('motivo_contato'
+, [1, 2])->whereBetween('created_at', ['2025-02-15 18:07:09', '2025-02-15 18:15:04'])
+->get();
--- ProjSuperGestao/app/Models/SiteContato.php
+++ ProjSuperGestao/app/Models/SiteContato.php
@@ -5,6 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Como o eloquent faz o processo para descobrir o nome da tabela no banco de dados
+// Site_Contato
+// site_contato
+// site_contatos
+
 class SiteContato extends Model
 {
     use HasFactory;
