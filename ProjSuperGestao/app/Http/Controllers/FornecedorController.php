74a89d09ed00706f5874f4ebbb343236887c2860
André Torres <150715327+andtors@users.noreply.github.com>
2025-02-21 15:50:15 -0300

    SC-12-156

--- ProjSuperGestao/app/Http/Controllers/FornecedorController.php
+++ ProjSuperGestao/app/Http/Controllers/FornecedorController.php
@@ -3,12 +3,60 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Fornecedor;
 
 class FornecedorController extends Controller
 {
     public function index(){
         
-        return view('app.fornecedor');
+        return view('app.fornecedor.index');
 
+    }
+
+    public function listar(Request $request){
+
+        $fornecedores = Fornecedor::where('nome', 'like', '%' . $request->input('nome') . '%')
+        ->where('email', 'like', '%' . $request->input('email') . '%')
+        ->where('site', 'like', '%' . $request->input('site') . '%')
+        ->where('uf', 'like', '%' . $request->input('uf') . '%')
+        ->where('email', 'like', '%' . $request->input('email') . '%')
+        ->get();
+
+    
+        return view('app.fornecedor.listar', ['fornecedores' => $fornecedores]);
+    }
+
+    public function adicionar(Request $request){
+
+        $msg = "";
+        echo ($request->getMethod());
+        if($request->getMethod() == 'POST'){
+          $regras = [
+            'nome' => 'required|min:3|max:40',
+            'site' => 'required',
+            'uf' => 'required|min:2|max:2',
+            'email' => 'email',
+          ];
+
+          $feedback = [
+            'required' => 'O campo: :attribute deve ser preenchido',
+            'nome.min' => 'O campo nome deve ter no minimo 3 caracteres',
+            'nome.max' => 'O campo nome deve ter no maximo 40 caracteres',
+            'uf.min' => 'O campo nome deve ter no minimo 2 caracteres',
+            'uf.max' => 'O campo nome deve ter no maximo 2 caracteres',
+            'email.email' => 'O campo e-mail não foi preenchido corretamente'
+          ];
+
+          $request->validate($regras, $feedback);
+
+          $fornecedor = new Fornecedor();
+          $fornecedor->create($request->all());
+
+          $msg = 'Cadastro realizado com sucesso';
+
+          return view('app.fornecedor.index', ['msg' => $msg]);
+        } else if ($request->getMethod() == 'GET'){
+            return view('app.fornecedor.adicionar', ['msg' => $msg]);
+        }
     }
 }
